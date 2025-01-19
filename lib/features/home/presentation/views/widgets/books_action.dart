90512ec643b77234afb29ff835f99daaf1e06e65
ayaaslama <ayamhmd089@gmail.com>
2025-01-19 14:01:24 +0200

    added books action

---
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/shared_widgets/custom_button.dart';
+
+class BooksAction extends StatelessWidget {
+  const BooksAction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+      child: Row(
+        children: [
+          Expanded(
+            child: CustomButton(
+              backgroundColor: Colors.white,
+              text: '16.99\$',
+              textColor: Colors.black,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12.r),
+                bottomLeft: Radius.circular(12.r),
+              ),
+            ),
+          ),
+          Expanded(
+            child: CustomButton(
+              backgroundColor: Color(0xffEF8262),
+              text: 'Free Preview',
+              textColor: Colors.white,
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(16.r),
+                bottomRight: Radius.circular(16.r),
+              ),
+              fontSize: 12.sp,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
