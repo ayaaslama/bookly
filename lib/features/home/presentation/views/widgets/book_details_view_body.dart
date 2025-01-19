d2a75b8eb5d0fd2678c6a0950cdcfda37791919e
ayaaslama <ayamhmd089@gmail.com>
2025-01-19 14:49:50 +0200

    refactor book details view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,10 +1,7 @@
 import 'package:bookly/core/helpers/spacing.dart';
-import 'package:bookly/core/shared_widgets/book_image_conrainer.dart';
-import 'package:bookly/core/theming/text_styles.dart';
-import 'package:bookly/features/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly/features/home/presentation/views/widgets/books_action.dart';
+import 'package:bookly/features/home/presentation/views/widgets/book_details_section.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
-import 'package:bookly/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -13,7 +10,6 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -23,45 +19,9 @@ class BookDetailsViewBody extends StatelessWidget {
             child: Column(
               children: [
                 CustomBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.17),
-                  child: BookImageContainer(),
-                ),
-                verticalSpace(40),
-                Text(
-                  'The Jungle Book',
-                  style: AppTextStyles.textStyle30Normal
-                      .copyWith(fontWeight: FontWeight.bold),
-                ),
-                verticalSpace(6),
-                Opacity(
-                  opacity: 0.7,
-                  child: Text(
-                    'Rudyard Kipling',
-                    style: AppTextStyles.textStyle18SemiBold.copyWith(
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ),
-                verticalSpace(18),
-                BookRating(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ),
-                verticalSpace(37),
-                BooksAction(),
+                BookDetailsSection(),
                 Expanded(child: verticalSpace(50)),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: AppTextStyles.textStyle14Normal.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                verticalSpace(16),
-                SimilarBooksListView(),
-                verticalSpace(40),
+                SimilarBooksSection(),
               ],
             ),
           ),
---
+++ lib/features/home/presentation/views/widgets/similar_books_section.dart
@@ -0,0 +1,26 @@
+import 'package:bookly/core/helpers/spacing.dart';
+import 'package:bookly/core/theming/text_styles.dart';
+import 'package:bookly/features/home/presentation/views/widgets/similar_books_list_view.dart';
+import 'package:flutter/material.dart';
+
+class SimilarBooksSection extends StatelessWidget {
+  const SimilarBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: AppTextStyles.textStyle14Normal.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        verticalSpace(16),
+        SimilarBooksListView(),
+        verticalSpace(40),
+      ],
+    );
+  }
+}
