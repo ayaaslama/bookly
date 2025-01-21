cc1f7929c17a68d580112bfb5d0453a0df011c04
ayaaslama <ayamhmd089@gmail.com>
2025-01-21 17:40:24 +0200

    create featured books state

--- lib/core/networking/api_constants.dart
+++ lib/core/networking/api_constants.dart
@@ -2,4 +2,6 @@ class ApiConstants {
   static const String apiBaseUrl = "https://www.googleapis.com/books/v1/";
   static const String newestBooksEndpoint =
       "https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest";
+  static const String allBooksEndpoint =
+      "https://www.googleapis.com/books/v1/volumes?Filtering=free-ebooks&q=subject:Programming";
 }
